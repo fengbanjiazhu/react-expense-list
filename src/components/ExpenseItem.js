ba6ee9b17ebc05cd6f352b944d53f2c65660a9db
fengbanjiazhu <562475805@qq.com>
2023-02-04 22:08:54 +1100

    first

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(data) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={data.data.date}></ExpenseDate>
+      <h2>{data.data.title}</h2>
+      <div className="expense-item__description">
+        <div className="expense-item__price">${data.data.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
