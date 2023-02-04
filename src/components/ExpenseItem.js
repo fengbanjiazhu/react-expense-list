6882579f383f3a2cc493f0a5d0e9b5977553a39d
fengbanjiazhu <562475805@qq.com>
2023-02-04 22:12:12 +1100

    id-try

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(data) {
   return (
-    <div className="expense-item">
+    <div className="expense-item" id="data.data.id">
       <ExpenseDate date={data.data.date}></ExpenseDate>
       <h2>{data.data.title}</h2>
       <div className="expense-item__description">
